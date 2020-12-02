d6b0e1fa688ef48626920673d5d7aec45836b3bb
J420s <aluseg230@ieselcaminas.org>
2020-12-02 22:22:39 +0100

    components

--- utils.php
+++ utils.php
@@ -1,5 +1,47 @@
 <?php
 
+function write_post_entries($dir){
+    if(is_dir($dir)&&$handler = opendir($dir)){
+        while (false !== ($file = readdir($handler))) {
+            if($file !== ".." && $file !== "."){
+                ?>
+                <!-----------------** Post Default **------------------->
+                <div class="row mt-5">
+                    <div class="col-md">
+                        <div class="card p-1">
+                            <div class="card-horizontal">
+                                <div class="img-square-wrapper">
+                                    <img class="border brounded" src="img/profile-default-small.jpg" alt="Card image cap">
+                                </div>
+                                <div class="card-body py-0">
+                                    <div class="row">
+                                        <div class="col-md">
+                                            <a href="">
+                                                <h4 class="card-title"><?decode($file)?></h4>
+                                            </a>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <form class="form-inline" >
+                                                <div class="form-group">
+                                                    <button type="button" class="btn btn-sm btn-primary my-1">Editar</button>
+                                                    <button type="button" class="btn btn-sm btn-danger ml-1 mr-1 my-1">Borrar</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-----------------** /Post Default **------------------->
+                <? 
+            }
+        }
+    }
+}
+
+
 function write_files_list(){
     $list = "<ul class='list-group' style='width:50%;'>";
     $dir='./posts/';
