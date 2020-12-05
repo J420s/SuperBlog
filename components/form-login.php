dff5e032c81005a25c9e03e0c55e12ba35fed68f
J420s <aluseg230@ieselcaminas.org>
2020-12-05 02:01:34 +0100

    templates

--- components/form-login.php
+++ components/form-login.php
@@ -1,16 +1,15 @@
 <form action="tools/login.php" method="POST" class="form-inline my-2 my-lg-0">
-    <div class="col pl-0">
+    <div class="col-md-5 pl-0">
         <div class="input-group">
             <input type="username" class="form-control" name="username" placeholder="Username">
         </div>
     </div>
-    <div class="col pl-0">
+    <div class="col-md-6 pl-0">
         <div class="input-group">
             <input type="password" class="form-control" name="password" placeholder="Password">
         </div>
     </div>
-    <div class="col pl-0 pr-0">
-        <button class="btn btn-primary my-2 my-sm-0" type="submit">Login</button>
-        <a class="btn btn-success my-2 my-sm-0" href="register.php">Register</a>
+    <div class="col-md-1 pl-0 pr-0 text-right">
+        <button class="btn btn-outline-primary btn-sm my-2 my-sm-0" type="submit">Login</button>
     </div>
 </form>
